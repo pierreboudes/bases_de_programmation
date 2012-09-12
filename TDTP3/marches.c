6bc9f1b73cc7529df6af9b9baf75c40dd7949e98
pierreboudes <boudes@univ-paris13.fr>
2012-09-12 21:20:19 +0000

    I3: semaine 3
    git-svn-id: https://www-lipn.univ-paris13.fr/svn/licences/trunk/UE_I3@668 04a62a05-9fe5-4f9b-8506-f2d01d604607

---
+++ TDTP3/marches.c
@@ -0,0 +1,49 @@
+/* Declaration de fonctionnalites supplementaires */
+#include <stdlib.h> /* pour EXIT_SUCCESS */
+#include <stdio.h> /* pour printf() */
+
+/* Declaration des constantes et types utilisateur */
+#define TRUE 1
+#define FALSE 0
+#define PANIQUE 2
+#define INQUIETUDE 1
+#define CONFIANCE 0
+
+/* Fonction principale */
+int main()
+{
+    /* Declaration et initialisation des variables */
+    int marches = PANIQUE;
+    int dette = 50; 
+    int refus = TRUE;
+    int rembourser = TRUE;
+    
+    if (marches == PANIQUE) /* 1) accroche toi a l'euribor je retire le zinzin */
+    {
+	rembourser = FALSE;
+    }
+    if (marches == INQUIETUDE) /* 2) c'est grave docteur ? */
+    {	
+	if (dette > 50)
+	{
+	    if (refus)
+	    {
+		rembourser = FALSE;
+	    }
+	}
+    }
+
+    if (rembourser)
+    {
+	printf("Ne rien changer\n");
+    }
+    else
+    {
+	printf("Ne plus rembourser\n");
+    }
+    
+    /* Valeur fonction */
+    return EXIT_SUCCESS;
+}
+
+/* Definition des fonctions utilisateur */
