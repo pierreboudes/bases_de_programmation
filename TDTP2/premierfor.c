8ee747a4e463ff9052b169b7a87ddd05403964a1
Pierre Boudes <boudes@univ-paris13.fr>
2014-11-03 08:46:55 +0100

    TP 2

---
+++ TDTP2/premierfor.c
@@ -0,0 +1,28 @@
+/* déclaration de fonctionalités supplémentaires */
+#include <stdlib.h> /* EXIT_SUCCESS */
+#include <stdio.h> /* printf */
+
+/* déclaration constantes et types utilisateurs */
+
+/* déclaration de fonctions utilisateurs */
+
+/* fonction principale */
+int main()
+{
+    /* déclaration et initialisation variables */
+    int i; /* variable de boucle */
+
+    un_bug;
+
+    for(i = 0; i < 5; i = i + 1)
+    {
+        printf("i = %d\n",i);
+    }
+    /* i >= 5 */
+
+    printf("i vaut %d après l'exécution de la boucle.\n",i);
+
+    return EXIT_SUCCESS;
+}
+
+/* definitions des fonctions utilisateurs */
